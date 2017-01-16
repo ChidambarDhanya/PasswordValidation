d15e520d32e7602a60716e1169f7cc8a09823b9c
DhanyaHegde <hegdedhanya021@gmail.com>
2017-01-16 17:48:51 +0530

    Dhanya

---
+++ PasswordVerification/Verification.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordVerification
+{
+    public class ValidationException : System.Exception
+    {
+        public ValidationException(string s) : base(s)
+        {
+
+        }
+    }
+    public class Verification
+    {
+            private string _password;
+            //private static int[] flag = new int[5];
+            public string Password
+            {
+                get { return _password; }
+                set { _password = value; }
+            }
+            public Verification()
+        {
+
+        }
+            private Verification(string password)
+            {
+                _password = password;
+            }
+        public bool Verify(string Password,object type)
+        {
+            bool ret= false;
+            if (type.GetType()==typeof(Internal))
+            {
+                Internal i=(Internal)type;
+               
+                ret = i.VerifyPassword(Password);
+            }
+            else if (type.GetType()==typeof(External))
+            {
+                External e = (External)type;
+                ret = e.VerifyPassword(Password);
+            }
+            else
+            {
+                //if (count >= 3)
+                //    return false;
+                //else 
+                Console.WriteLine("ERROR");
+            }
+            return ret;
+
+        }
+
+
+        public class External: Verification
+        {
+            public External():base()
+            {
+            }
+            public bool VerifyPassword(string Password)
+            {
+                int count = 0;
+                try
+                {
+                    VerifyNull(Password);
+                }
+                catch (ValidationException e)
+                {
+                    count++;
+                }
+                try
+                {
+                    VerifyLength(Password);
+                }
+                catch (ValidationException e)
+                {
+                    count++;
+                }
+                try
+                {
+                    VerifyLower(Password);
+                }
+                catch (ValidationException e)
+                {
+                    count++;
+                }
+                try
+                {
+                    VerifyUpper(Password);
+                }
+                catch (ValidationException e)
+                {
+                    count++;
+                }
+                try
+                {
+                    VerifyNumber(Password);
+                }
+                catch (ValidationException e)
+                {
+                    count++;
+                }
+                if (count >= 3)
+                    return false;
+                else return true;
+            }
+            public static bool VerifyNull(string Password)
+            {
+                if (string.IsNullOrEmpty(Password))
+                {
+                    //flag[0] = 1;
+                    throw new ValidationException("Password can't be empty");
+                    // return false;
+                }
+                return true;
+            }
+            public static bool VerifyUpper(string Password)
+            {
+
+                if (!(Password.Any(c => char.IsUpper(c))))
+                {
+                    //flag[2] = 1;
+                    throw new ValidationException("Password should have at least one upper case letter");
+                }
+                return true;
+            }
+
+            public static bool VerifyLower(string Password)
+            {
+
+                if (!(Password.Any(c => char.IsLower(c))))
+                {
+                    //flag[2] = 1;
+                    throw new ValidationException("Password should have at least one Lower case letter");
+                }
+                return true;
+            }
+            public static bool VerifyNumber(string Password)
+            {
+
+                if (!(Password.Any(c => char.IsNumber(c))))
+                {
+                    //flag[2] = 1;
+                    throw new ValidationException("Password should have at least one number");
+                }
+                return true;
+            }
+            public static bool VerifyLength(string Password)
+            {
+
+                if (Password.Length <= 8)
+                {
+                    //flag[2] = 1;
+                    throw new ValidationException("password should have minimum of 8 charcaters");
+                }
+                return true;
+            }
+        }
+        public class Internal : Verification
+        {
+            public Internal() : base()
+            {
+            }
+            
+            public bool VerifyPassword(string Password)
+            {
+                if (Password.Length > 8)
+                    return true;
+                else return false;
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                //PasswordValidator.Verify(Console.ReadLine());
+                //Console.WriteLine(Verification.Verify(Console.ReadLine()));
+                // object i = new Internal("dhan");
+                //Console.WriteLine(i.GetType());
+                string password = Console.ReadLine();
+                //Internal e = new Internal();
+                External e = new External();
+                Verification v = new Verification();
+                v.Verify(password, e);
+                Console.WriteLine(v.Verify(password,e));
+                Console.ReadKey();
+            }
+        }
+    }
+    }
+
---
+++ VerificationTest/UnitTest1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PasswordVerification;
+
+namespace VerificationTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void InternalVerifyTestForPasswordFalse()
+        {
+            bool result = false;
+            Verification v = Construct();
+            Verification.Internal i = new Verification.Internal();
+            Assert.AreEqual(result, v.Verify("dH1",i), "error");
+           
+        }
+
+        [TestMethod]
+        public void InternalVerifyTestForPasswordTrue()
+        {
+            bool result = true;
+            Verification v = Construct();
+            Verification.Internal i = new Verification.Internal();
+
+            Assert.AreEqual(result, v.Verify("dhanya123456", i), "error");
+
+        }
+
+        private static Verification Construct()
+        {
+            return new Verification();
+        }
+
+
+        [TestMethod]
+        public void ExternalVerificationTestForNull()
+        {
+
+            //  PasswordValidator.Verify("");
+            try
+            {
+                Verification v = Construct();
+                Verification.External e = new Verification.External();
+                v.Verify("",e);
+            }
+            catch (ValidationException e)
+            {
+                Assert.AreEqual("Password can't be empty", e.Message, "error");
+            }
+
+        }
+        [TestMethod]
+        public void ExternalVerificationTestForUpperCase()
+        {
+
+            try
+            {
+               // Verification.VerifyUpper("dsfgjhdsfjk");
+                Verification v = Construct();
+                Verification.External e = new Verification.External();
+                v.Verify("dsfgjhdsfjk", e);
+            }
+            catch (ValidationException e)
+            {
+                Assert.AreEqual("Password should have at least one upper case letter", e.Message, "error");
+            }
+
+
+        }
+        [TestMethod]
+        public void ExternalVerificationTestForLowerCase()
+        {
+            try
+            {
+                //Verification.VerifyLower("SGFKSGFUK");
+                Verification v = Construct();
+                Verification.External e = new Verification.External();
+                //string s = null;
+                string s = "";
+                v.Verify("GFKSGFUK", e);
+
+            }
+            catch (ValidationException e)
+            {
+                Assert.AreEqual("Password should have at least one Lower case letter", e.Message, "error");
+            }
+
+        }
+
+        [TestMethod]
+        public void ExternalVerificationTestForNumber()
+        {
+            try
+            {
+                //Verification.VerifyNumber("fvgkysdAFE");
+                Verification v = Construct();
+                Verification.External e = new Verification.External();
+                v.Verify("fvgkysdAFE", e);
+            }
+            catch (ValidationException e)
+            {
+                Assert.AreEqual("Password should have at least one number", e.Message, "error");
+            }
+
+        }
+        [TestMethod]
+        public void ExternalVerificationTestForLength()
+        {
+            try
+            {
+               // Verification.VerifyLength("fvVk1");
+                Verification v = Construct();
+                Verification.External e = new Verification.External();
+                v.Verify("fvVk1", e);
+            }
+            catch (ValidationException e)
+            {
+                Assert.AreEqual("password should have minimum of 8 charcaters", e.Message, "error");
+            }
+
+        }
+        [TestMethod]
+        public void ExternalVerificationVerifyTestForPasswordFalse()
+        {
+            bool result = false;
+            Verification v = Construct();
+            Verification.External e = new Verification.External();
+            //v.Verify("fvVk1", e);
+            Assert.AreEqual(result, v.Verify("dha",e), "error");
+        }
+        [TestMethod]
+        public void ExternalVerificationVerifyTestForPasswordTrue()
+        {
+            bool result = true;
+            Verification v = Construct();
+            Verification.External e = new Verification.External();
+            //v.Verify("fvVk1", e);
+            Assert.AreEqual(result, v.Verify("dhaA", e), "error");
+        }
+
+        [TestMethod]
+        public void ExternalVerificationVerifyTestForPasswordTrueAllCondition()
+        {
+            bool result = true;
+            Verification v = Construct();
+            Verification.External e = new Verification.External();
+            //v.Verify("fvVk1", e);
+            Assert.AreEqual(result, v.Verify("dhaA1asgsd", e), "error");
+        }
+
+    }
+
+}
